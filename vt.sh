fdeb00e6781fd99f0aa31a0c56af5855d38d60d7
q4artz <116222352+q4artz@users.noreply.github.com>
2025-02-23 04:55:06 +0800

    Add files via upload

--- vt.sh
+++ vt.sh
@@ -1,36 +1,57 @@
 #!/bin/bash
 
-
 echo -e "This Script Takes in | domain (provide txt file if more than one) |\n"
-echo -e "./vt.sh ipaddr.txt \n"
+echo -e "\n ./vt.sh data.txt ip \n"
+echo -e "\n ./vt.sh data.txt domain \n"
 
 #args
-ipaddr=$1
+data=$1
+type=$2
+APIKEY='ENTER API KEY HEREEEE!!!!'
 
-# creating required directories
+# Api need to be used
+IPGet(){
+	curl --request GET \
+		--url https://www.virustotal.com/api/v3/ip_addresses/$line \
+		--header 'accept: application/json' \
+		--header 'x-apikey:'."$APIKEY" \
+		> $pwd/resultfolder/out.json
+}
+DomainGet(){}
+URLGet(){}
+HashGet(){}
+
+# creating required directory
 pwd=$(pwd)
 
 if [ ! -d "resultfolder" ]; then
 	mkdir $pwd/resultfolder
 else 
-	echo -e "[+] file already exist \n"
+	echo -e "[+] folder already exist \n"
 fi
 
-# assigning Ips/Domains to vars
-if [ -f $ipaddr ]; then
-	IFS=$'\r\n' GLOBIGNORE='*' command eval  'IPs=($(cat $ipaddr))'
+# assigning Datas to var
+if [ -f $data ]; then
+	IFS=$'\r\n' GLOBIGNORE='*' command eval  'Datas=($(cat $data))'
 fi
 
 # running vt itself
 echo -e "[+] running IPs through Virustotal \n"
 
-for line in "${IPs[@]}"
-do
-	curl --request GET \
-		--url https://www.virustotal.com/api/v3/ip_addresses/$line \
-		--header 'accept: application/json' \
-		--header 'x-apikey: [API KEY HEREEEEE!!!]' \
-		> $pwd/resultfolder/out.json
-done
+case "{$type,,}" in
+
+	ip)
+	for line in "${Datas[@]}" ; do IPGet() $line $APIKEY $pwd ; done 
+	;;
+	domain)
+	for line in "${Datas[@]}" ; do DomainGet() $line $APIKEY $pwd ; done 
+	;;
+	url)
+	for line in "${Datas[@]}" ; do URLGet() $line $APIKEY $pwd ; done 
+	;;
+	"hash")
+	for line in "${Datas[@]}" ; do HashGet() $line $APIKEY $pwd ; done 
+	;;
+	esac
 
 echo '[+] scan has finished!!!!'
