eb6cce27dbe441a0855b64c02a071a55bc2c001e
q4artz <116222352+q4artz@users.noreply.github.com>
2025-02-26 04:12:10 +0800

    Update vt.sh

--- vt.sh
+++ vt.sh
@@ -1,55 +1,82 @@
 #!/bin/bash
  
 echo -e "This Script Takes in | IP/Domain/Hash/URL (provide txt file if more than one) |\n"
-echo -e "\n ./vt.sh data.txt ip \n"
-echo -e "\n ./vt.sh data.txt domain \n"
+echo -e "\n ./vt.sh data.txt ip scan \n"
+echo -e "\n ./vt.sh data.txt domain rescan \n"
  
 #args
 data=$1
 type=$2
+scan=$3
 APIKEY="Enter API Key HERE!!!!!"
- 
+
+if [ "${data,,}" = "help" ]; then
+    echo "help"
+fi
+
 # API need to be used
 IPGet(){
     curl --request GET \
-		--url https://www.virustotal.com/api/v3/ip_addresses/$line \
-		--header 'accept: application/json' \
-		--header 'x-apikey:'. "$APIKEY" \
-		> $pwd/resultfolder/out.json
+    		--url https://www.virustotal.com/api/v3/ip_addresses/$line \
+    		--header 'accept: application/json' \
+    		--header 'x-apikey:'. "$APIKEY" \
+    		> $pwd/resultfolder/out.json
 }
- 
 DomainGet(){
     curl --request GET \	
     		--url https://www.virustotal.com/api/v3/domains/$line \
      		--header 'accept: application/json' \
-	 	--header 'x-apikey:'. "$APIKEY" \
-		> $pwd/resultfolder/out.json
+    	 	--header 'x-apikey:'. "$APIKEY" \
+    		> $pwd/resultfolder/out.json
 }
- 
 URLGet(){
     curl --request GET \
      		--url https://www.virustotal.com/api/v3/urls/$line \
        		--header 'accept: application/json' \
-	 	--header 'x-apikey:'. "$APIKEY" \
-		> $pwd/resultfolder/out.json
+    	 	--header 'x-apikey:'. "$APIKEY" \
+    		> $pwd/resultfolder/out.json
 }
 HashGet(){
     curl --request GET \
      		--url https://www.virustotal.com/api/v3/files/$line \
-	 	--header 'accept: application/json' \
-   		--header 'x-apikey:'. "$APIKEY" \
-		> $pwd/resultfolder/out.json
+    	 	--header 'accept: application/json' \
+       		--header 'x-apikey:'. "$APIKEY" \
+    		> $pwd/resultfolder/out.json
 }
+IPRescan(){
+    curl --request POST \
+             --url https://www.virustotal.com/api/v3/ip_addresses/$line/analyse \
+             --header 'accept: application/json' \
+             --header 'x-apikey:'. "$APIKEY" \
+}
+DomainRescan(){
+    curl --request POST \
+             --url https://www.virustotal.com/api/v3/domains/$line/analyse \
+             --header 'accept: application/json' \
+             --header 'x-apikey:'. "$APIKEY" \
+}
+URLRescan(){
+    curl --request POST \
+             --url https://www.virustotal.com/api/v3/urls/$line/analyse \
+             --header 'accept: application/json' \
+             --header 'x-apikey:'. "$APIKEY" \
+}
+HashRescan(){
+    curl --request POST \
+             --url https://www.virustotal.com/api/v3/files/$line/analyse \
+             --header 'accept: application/json' \
+             --header 'x-apikey:'. "$APIKEY" \
+}
+
+# ---------------------------------------------------------------------------------------------------------------
  
 # creating required directories
 pwd=$(pwd)
  
 if [ ! -d "resultfolder" ]; then
 	mkdir $pwd/resultfolder
-else 
-	echo -e "[+] file already exist \n"
 fi
- 
+
 # assigning Ips/Domains to vars
 if [ -f $data ]; then
 	IFS=$'\r\n' GLOBIGNORE='*' command eval  'Datas=($(cat $data))'
@@ -57,22 +84,38 @@ fi
  
 # Running VT chekcs
 echo -e "[+] running data through Virustotal \n"
-case "{$type,,}" in
+case "${type,,}" in
  
   ip)
-    for line in "${Datas[@]}" ; do IPGet $line $APIKEY $pwd ; done
+    if [ "${scan,,}" != "scan" ]; then
+            for line in "${Datas[@]}" ; do IPRescan $scan $line $APIKEY ; done
+    else
+            for line in "${Datas[@]}" ; do IPGet $line $APIKEY $pwd ; done
+    fi
     ;;
  
   domain)
-    for line in "${Datas[@]}" ; do DomainGet $line $APIKEY $pwd ; done
+    if [ "${scan,,}" != "scan" ]; then
+            for line in "${Datas[@]}" ; do DomainRescan $line $APIKEY ; done
+    else
+            for line in "${Datas[@]}" ; do DomainGet $line $APIKEY $pwd ; done
+    fi
     ;;
  
   url)
-    for line in "${Datas[@]}" ; do URLGet $line $APIKEY $pwd ; done
+    if [ "${scan,,}" != "scan" ]; then
+            for line in "${Datas[@]}" ; do URLRescan $line $APIKEY ; done
+    else
+            for line in "${Datas[@]}" ; do URLGet $line $APIKEY $pwd ; done
+    fi
     ;;
  
-  "hash")
-    for line in "${Datas[@]}" ; do HashGet $line $APIKEY $pwd ; done
+  hash)
+    if [ "${scan,,}" != "scan" ]; then
+            for line in "${Datas[@]}" ; do HashRescan $line $APIKEY ; done
+    else
+            for line in "${Datas[@]}" ; do HashGet $line $APIKEY $pwd ; done
+    fi
     ;;
 esac
  
